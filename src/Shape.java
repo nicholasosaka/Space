a697476f5fdf4bcb0d01560391b650435c675ab3
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-07 19:56:48 -0400

    shape constructors

--- src/Shape.java
+++ src/Shape.java
@@ -3,4 +3,29 @@
  * @date 9/7/2019
  */
 public class Shape {
+	private int id, panelSizeY, panelSizeX, width, height, locX, locY, radius;
+	private int[] x, y;
+
+	Shape(int id, int panelSizeX, int panelSizeY, int locY, int locX, int width, int height) {
+		this(id, locY, locX, width, height);
+		this.panelSizeX = panelSizeX;
+		this.panelSizeY = panelSizeY;
+	}
+
+	Shape(int id, int locY, int locX, int width, int height){
+		this.id = id;
+		this.locY = locY;
+		this.locX = locX;
+		this.width = width;
+		this.height = height;
+	}
+
+	Shape(int id, int panelSizeX, int panelSizeY, int locY, int locX, int radius){
+		this.id = id;
+		this.locY = locY;
+		this.locX = locX;
+		this.panelSizeX = panelSizeX;
+		this.panelSizeY = panelSizeY;
+		this.radius = radius;
+	}
 }
