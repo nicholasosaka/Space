b14cff8682f66bd532347a7433686a0e847d689d
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-07 19:35:50 -0400

    Space constructor

--- src/Space.java
+++ src/Space.java
@@ -1,6 +1,21 @@
+import javax.swing.*;
+
 /*
  * @author Nikko Osaka
  * @date 9/7/2019
  */
 public class Space {
+	private JFrame frame;
+	private Panel panel;
+
+	Space(String title, int width, int length){
+		this.frame = new JFrame(title);
+		this.frame.setSize(width, length);
+		this.panel = new Panel(this.frame);
+		this.frame.add(this.panel);
+		this.frame.pack();
+		this.frame.repaint();
+		this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.frame.setVisible(true);
+	}
 }
