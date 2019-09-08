8586cc2b941d27140b3228de305ab0cc510bcf25
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-07 22:24:33 -0400

    first test to check if addShape works

--- tests/SpaceTest.java
+++ tests/SpaceTest.java
@@ -1,13 +1,15 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SpaceTest {
 
-	@BeforeEach
-	void setUp() {
-	}
-
-	@AfterEach
-	void tearDown() {
+	@Test
+	void addShapeShouldAddShape() {
+		Space space = new Space("addShapeShouldAddShape", 500, 500);
+		Shape s = new Shape(1,100,100,100,100);
+		space.addShape(s);
+		assertEquals(1, space.getShapes().size());
+		assertEquals(s, space.getShapes().get(0));
 	}
 }
