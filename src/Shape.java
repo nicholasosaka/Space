90d8dc928758d6443457b531bf94f9985578aa79
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-07 20:11:48 -0400

    x and y refactor

--- src/Shape.java
+++ src/Shape.java
@@ -6,13 +6,13 @@ public class Shape {
 	private int id, panelSizeY, panelSizeX, width, height, locX, locY, radius;
 	private int[] x, y;
 
-	Shape(int id, int panelSizeX, int panelSizeY, int locY, int locX, int width, int height) {
+	Shape(int id, int panelSizeX, int panelSizeY, int locX, int locY, int width, int height) {
 		this(id, locY, locX, width, height);
 		this.panelSizeX = panelSizeX;
 		this.panelSizeY = panelSizeY;
 	}
 
-	Shape(int id, int locY, int locX, int width, int height){
+	Shape(int id, int locX, int locY, int width, int height){
 		this.id = id;
 		this.locY = locY;
 		this.locX = locX;
@@ -20,7 +20,7 @@ public class Shape {
 		this.height = height;
 	}
 
-	Shape(int id, int panelSizeX, int panelSizeY, int locY, int locX, int radius){
+	Shape(int id, int panelSizeX, int panelSizeY, int locX, int locY, int radius){
 		this.id = id;
 		this.locY = locY;
 		this.locX = locX;
