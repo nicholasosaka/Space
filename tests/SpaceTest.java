51c68e75aa3443d177914d530acff82497786172
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-07 21:37:35 -0400

    create test scaffold for Space class

---
+++ tests/SpaceTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class SpaceTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+}
