b849a47f71040d4c7541f1ffb5b94a56ad9dcc3c
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-19 18:41:13 -0400

    utilize @Before annotation

--- src/test/java/SpaceTest.java
+++ src/test/java/SpaceTest.java
@@ -1,13 +1,18 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SpaceTest {
+	Space space;
+
+	@Before
+	public void initialize(){
+		space = new Space("Unit Testing", 500, 500);
+	}
 
 	@Test
 	public void addShapeShouldAddShape() {
-		Space space = new Space("addShapeShouldAddShape", 500, 500);
-
 		Shape s = new Shape(1,100,100,100,100);
 		space.addShape(s);
 		assertEquals(s, space.getShapes().get(0));
@@ -18,8 +23,6 @@ public class SpaceTest {
 
 	@Test
 	public void addCircle() {
-		Space space = new Space("addCircle", 500, 500);
-
 		Shape s = new Shape(4,500,250,250,100);
 		space.addShape(s);
 		assertEquals(4, space.getShapes().get(0).getID());
@@ -27,8 +30,6 @@ public class SpaceTest {
 
 	@Test
 	public void addTriangle() {
-		Space space = new Space("addTriangle", 500, 500);
-
 		Shape s = new Shape(0,500,250,250,100);
 		space.addShape(s);
 		assertEquals(0, space.getShapes().get(0).getID());
@@ -36,8 +37,6 @@ public class SpaceTest {
 
 	@Test
 	public void addRectangle() {
-		Space space = new Space("addCircle", 500, 500);
-
 		Shape s = new Shape(2,500,250,250,100);
 		space.addShape(s);
 		assertEquals(2, space.getShapes().get(0).getID());
@@ -45,20 +44,15 @@ public class SpaceTest {
 
 	@Test
 	public void multipleShapesShouldPopulateProperly() {
-		Space space = new Space("multipleShapesShouldPopulateProperly", 500,500);
-
 		for(int i = 0; i < 5; i++) {
 			space.addShape(i % 4, 100, 100, 100, 100);
 		}
 
 		assertEquals(5, space.getShapes().size());
-
 	}
 
 	@Test
 	public void pointCheckForTriangle(){
-		Space space = new Space("pointCheckForTriangle", 500, 500);
-
 		space.addShape(0,250,250,100,100);
 
 		assertTrue(space.isPointInSpace(250, 250));
@@ -68,8 +62,6 @@ public class SpaceTest {
 
 	@Test
 	public void pointCheckForCircle(){
-		Space space = new Space("pointCheckForCircle", 500, 500);
-
 		space.addShape(250, 250, 100);
 
 		assertTrue(space.isPointInSpace(250, 250));
@@ -79,8 +71,6 @@ public class SpaceTest {
 
 	@Test
 	public void pointCheckForRectangle(){
-		Space space = new Space("pointCheckForRectangle", 500, 500);
-
 		space.addShape(2,250, 250, 100,100);
 
 		assertTrue(space.isPointInSpace(250, 250));
@@ -90,7 +80,6 @@ public class SpaceTest {
 
 	@Test
 	public void pointInSpaceTrue(){
-		Space space = new Space("pointInSpaceTrue", 500, 500);
 		Shape p = new Shape(3, 500, 500, 250, 250, 5);
 		space.addShape(250, 250, 100);
 
@@ -101,7 +90,6 @@ public class SpaceTest {
 
 	@Test
 	public void pointInSpaceFalse(){
-		Space space = new Space("pointInSpaceFalse", 500, 500);
 		Shape p = new Shape(3, 500, 500, 5, 5, 5);
 		space.addShape(250, 250, 50);
 
@@ -112,11 +100,9 @@ public class SpaceTest {
 
 	@Test
 	public void checkSpaceContents(){
-		Space space = new Space("checkSpaceContents", 500, 500);
 		Shape s = new Shape(3, 500, 500, 250, 250, 100);
 		Shape q = new Shape(3, 500, 500, 250, 250, 100);
 		space.addShape(s);
-
 
 		assertTrue(space.isShapeInSpace(s));
 		assertTrue(space.isShapeInSpace(q));
