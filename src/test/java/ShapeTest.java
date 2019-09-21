1c7cde434739e4d9cbe32b79ed99a18b7c9bee35
Nikko Osaka <nikko.osaka99@gmail.com>
2019-09-21 17:26:31 -0400

    fix exception throw

--- src/test/java/ShapeTest.java
+++ src/test/java/ShapeTest.java
@@ -8,7 +8,7 @@ public class ShapeTest {
 	Space space;
 	Shape a,b;
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
 		space = new Space("Unit testing", 500, 500);
 		a = new Shape(3,100,100,100);
 		b = new Shape(3,400,400,100);
