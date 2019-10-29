759396064934287ec85a4998ed841d145df2bab5
Nikko Osaka <nikko.osaka99@gmail.com>
2019-10-29 10:37:35 -0400

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,6 +4,7 @@
  */
 public class Driver {
 	public static void main(String[] args){
-
+		SpaceGame myGame = new SpaceGame("myGame window!", 500, 500);
+		System.out.println("We made a game window!");
 	}
 }
